727bde8687599cd8daa9c610b1e842b66f464b40
xuejin13 <xuejingao@gmail.com>
2019-11-01 03:03:12 -0400

    Created Twitter 2.0

---
+++ Twitter/TweetTableViewCell.swift
@@ -0,0 +1,30 @@
+//
+//  TweetTableViewCell.swift
+//  Twitter
+//
+//  Created by Xuejin Gao on 11/1/19.
+//  Copyright © 2019 Dan. All rights reserved.
+//
+
+import UIKit
+
+class TweetTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var tweetContent: UILabel!
+    
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
